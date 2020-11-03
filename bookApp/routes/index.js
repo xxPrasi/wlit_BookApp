1a44ec0c8c9403bfc5e44d407f0500228dc87ea7
Prasi <72287704+xxPrasi@users.noreply.github.com>
2020-11-03 10:48:20 +0545

    first commit

---
+++ bookApp/routes/index.js
@@ -0,0 +1,10 @@
+var express = require('express');
+var router = express.Router();
+var books = require('../resources/books')
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Book DB',bookList: books});
+});
+
+module.exports = router;
